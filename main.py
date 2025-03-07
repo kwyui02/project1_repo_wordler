99ac85a7d51aa1afab050cad92b258f743f43421
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-03-08 00:53:26 +0800

    reorganized to validate_word function

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import re
 
+
 guess = ['_', '_', '_', '_', '_']
 valid_length = 5
-
-def input_word() -> str:
-    return input("Enter your word: \n").upper()
 
 
 def input_letter(letter: str) -> str:
@@ -24,27 +22,50 @@ def print_guide() -> None:
     print("GREEN = right letter, right position\n")
 
 
-def word_length_check(guess_word: str) -> int:
-    """ 
-        checks if the input word is 5 letters long, returns:
-        1 if the word is valid
-        0 if the word is too long
-        -1 if the word is too short
-    """
-    if len(guess_word) == valid_length:
-        return 1
-    elif len(guess_word) > valid_length:
-        return 0
-    else:
-        return -1
+def validate_word() -> str:
+    def input_word() -> str:
+        return input("Enter your word: \n").upper()
+    
+    def word_length_check(guess_word: str) -> int:
+        """ 
+            checks if the input word is 5 letters long, returns:
+            1 if the word is valid
+            0 if the word is too long
+            -1 if the word is too short
+        """
+        if len(guess_word) == valid_length:
+            return 1
+        elif len(guess_word) > valid_length:
+            return 0
+        else:
+            return -1
 
+    def char_check(guess_word: str) -> bool:
+        """
+            checks for any special characters in the input word
+            returns True if there are no special characters
+        """
+        return re.search(r"^[A-Za-z]*$", guess_word) is not None
 
-def char_check(guess_word: str) -> bool:
-    """
-        checks for any special characters in the input word
-        returns True if there are no special characters
-    """
-    return re.search(r"^[A-Za-z]*$", guess_word) is not None
+    guess_word = input_word()
+    while True:
+        is_right_length = word_length_check(guess_word)
+        if is_right_length == 1:
+            break
+        if is_right_length == 0:
+            print("The word is too long!\n")
+        else:
+            print("The word is too short!\n")
+        guess_word = input_word()
+
+    while True:
+        has_no_characters = char_check(guess_word)
+        if has_no_characters:
+            break
+        print("Invalid input. Please remove any special characters.")
+        guess_word = input_word()
+
+    return guess_word
 
 
 def database(word: str, grays: str, yellows: 
@@ -69,10 +90,6 @@ def value_check(index: int, grays: str,
         return r"[^{grays}{yellow}]".format(grays=grays, yellow=yellows[index])
     else:
         return value
-
-
-def last_char_is_yellow(yellows: dict[str, int]) -> bool:
-    return yellows[4] != ""
 
 
 def guesser(grays: str, yellows: dict[str, int]) -> list[str]:
@@ -123,32 +140,15 @@ def main() -> None:
         4: ""
     }
     is_solved = False
-    guess_num = 1
+    guess_ctr = 1
 
     print_guide()
     while not is_solved:
-        if guess_num == 1:
+        if guess_ctr == 1:
             print("It's your first word! "
                 "Try beginning with the word CRANE :)")
-        guess_word = input_word()
 
-        while True:
-            is_right_length = word_length_check(guess_word)
-            if is_right_length == 1:
-                break
-            if is_right_length == 0:
-                print("The word is too long!\n")
-            else:
-                print("The word is too short!\n")
-            guess_word = input_word()
-
-        while True:
-            has_no_characters = char_check(guess_word)
-            if has_no_characters:
-                break
-            print("Invalid input. Please remove any special characters.")
-            guess_word = input_word()
-
+        guess_word = validate_word()
         grays, yellows = database(guess_word, grays, yellows)
         guesses = guesser(grays, yellows)
         guesses_num = len(guesses)
@@ -164,10 +164,10 @@ def main() -> None:
                     is_solved = True
                 elif solve_input == "NO":
                     is_solved = False
-                    guess_num += 1
+                    guess_ctr += 1
                 break
 
-    print(f"\nCongratulations! You have solved the wordle in {guess_num + 1} " 
+    print(f"\nCongratulations! You have solved the wordle in {guess_ctr + 1} " 
           "tries!")
 
 
